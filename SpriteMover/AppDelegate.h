d0888f446406ac4153ca3bce931f91c9509f430a
Albert Villanueva Carreras <albertvc10@gmail.com>
2014-10-16 09:36:02 +0200

    Initial Commit

---
+++ SpriteMover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteMover
+//
+//  Created by Albert Villanueva Carreras on 16/10/14.
+//  Copyright (c) 2014 Albert Villanueva Carreras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
